8eeeb84d4df6c97daa1cef344b075bbab8d27d11
Shashank Sahni <shredder12@gmail.com>
2013-09-27 22:39:17 +0530

    Autodiscovery failure through search resolved
    For unknown reasons, the search resource doesn't return results at
    times. This patch fixes it by querying the service in loop unti
    timeout.
    
    Signed-off-by: Shashank Sahni <shredder12@gmail.com>

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+# Timeout value for service discovery
+
+default['hadooptack']['discovery']['timeout'] = 30
--- recipes/slave.rb
+++ recipes/slave.rb
@@ -4,6 +4,24 @@ node_name = node.name
 cluster_id = node.name.split('-')[0]
 
 master_node_array = search(:node, "name:#{cluster_id}-master")
+count = 0
+
+# At time search module doesn't return the correct result. This loop
+# is to mitigate that failure.
+
+while master_node_array.length < 1
+	log "waiting for master discovery"
+	sleep(1)
+	count += 1
+	master_node_array = search(:node, "name:#{cluster_id}-master")
+
+	if count == node['hadoopstack']['discovery']['timeout']
+		log "master discovery timeout!!"
+		break
+	end
+end
+
+log "master node array #{master_node_array} #{master_node_array.length}"
 
 master_node_array.each do |master|
 	node.set['hadoop']['namenode'] = master['ipaddress']
